84cc48c0e012453ae3555f37302bc70bd8771578
Asuka Ochi <asukaochi@gmail.com>
2017-01-09 19:07:28 +0000

    implement create restaurants

--- yelp_clone/app/controllers/restaurants_controller.rb
+++ yelp_clone/app/controllers/restaurants_controller.rb
@@ -1,5 +1,20 @@
 class RestaurantsController < ApplicationController
+
   def index
     @restaurants = Restaurant.all
   end
+
+  def new
+    @restaurants = Restaurant.new
+  end
+
+  def create
+    @restaurants = Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
+
 end
