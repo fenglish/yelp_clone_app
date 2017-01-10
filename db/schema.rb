41336f0c5558df35ebe34b0f12cf704758854576
Asuka Ochi <asukaochi@gmail.com>
2017-01-10 13:45:05 +0000

    the latest commit before restart

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,4 @@ ActiveRecord::Schema.define(version: 20170110125237) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "restaurants", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "rating"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
 end
